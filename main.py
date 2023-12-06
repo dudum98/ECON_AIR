b6be45e9814d00545ad0dc14b93bd27bb7d53148
Nethmee Perera <62570762+dudum98@users.noreply.github.com>
2023-12-05 21:44:11 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,43 @@
 import matplotlib.pyplot as plt
 
 class Flight:
-    def __init__(self, number, distance, price, duration, layovers):
-        self.number = number
+    def __init__(self, flight_number, distance, ticket_price, duration, layovers):
+        self.flight_number = flight_number
         self.distance = distance
-        self.price = price
+        self.ticket_price = ticket_price
         self.duration = duration
         self.layovers = layovers
 
 def efficient_flight(flights):
     total_distance = 0
-    total_price = 0
+    total_cost = 0
 
     for flight in flights:
-        if total_distance + flight.distance <= 1000:
+        if total_distance + flight.distance <= 2000:
             total_distance += flight.distance
-            total_price += flight.price
+            total_cost += flight.ticket_price
 
+    # Return the last selected flight == the best
     return flights[-1]
 
 def visualize_data(flights):
-    ratios = [flight.price / flight.distance for flight in flights]
-    flight_numbers = [flight.number for flight in flights]
+    ratios = [flight.ticket_price / flight.distance for flight in flights]
+    flight_numbers = [flight.flight_number for flight in flights]
 
-    plt.figure(figsize=(8, 6))
+    plt.figure(figsize=(2, 6))
     plt.bar(flight_numbers, ratios, color='skyblue')
     plt.xlabel('Flight Number')
     plt.ylabel('Cost-Distance Ratio')
     plt.title('Flight Cost-to-Distance Ratio Visualization')
-    plt.savefig('static/flight_visualization.png')
+    plt.show()
 
 def bubble_sort(flights):
     n = len(flights)
 
     for i in range(n - 1):
         for j in range(0, n - i - 1):
-            ratio_j = flights[j].price / flights[j].distance
-            ratio_j1 = flights[j + 1].price / flights[j + 1].distance
+            ratio_j = flights[j].ticket_price / flights[j].distance
+            ratio_j1 = flights[j + 1].ticket_price / flights[j + 1].distance
 
             if ratio_j > ratio_j1:
                 flights[j], flights[j + 1] = flights[j + 1], flights[j]
@@ -58,33 +59,35 @@ def main():
     num_flights = int(input("Enter the number of flights: "))
 
     for i in range(num_flights):
-        number = input(f"Enter Flight {i + 1} Number: ")
+        flight_number = input(f"Enter Flight {i + 1} Number: ")
         distance = int(input(f"Enter Distance for Flight {i + 1} (in km): "))
-        price = float(input(f"Enter Ticket Price for Flight {i + 1} (in $): "))
+        ticket_price = float(input(f"Enter Ticket Price for Flight {i + 1} (in $): "))
         duration = float(input(f"Enter Duration for Flight {i + 1} (in hours): "))
         layovers = int(input(f"Enter Number of Layovers for Flight {i + 1}: "))
 
-        flights.append(Flight(number, distance, price, duration, layovers))
+        flights.append(Flight(flight_number, distance, ticket_price, duration, layovers))
 
     # Bubble sort flights based on the lowest cost-to-distance ratio
     bubble_sort(flights)
 
     # Display the sorted data
-    sorted_data = []
+    print("\nSorted Data (Lowest Cost-to-Distance Ratio):")
     for flight in flights:
-        sorted_data.append(f"Flight {flight.number}: {flight.price / flight.distance:.4f}")
+        print(f"Flight {flight.flight_number}: {flight.ticket_price / flight.distance:.4f}")
 
     # Greedy selection
     selected_flight = efficient_flight(flights)
 
-    # Display the result
-    selected_flight_info = (
-        f"Flight Number: {selected_flight.number}\n"
-        f"Distance: {selected_flight.distance} km\n"
-        f"Ticket Price: ${selected_flight.price}\n"
-        f"Duration: {selected_flight.duration} hours\n"
-        f"Layovers: {selected_flight.layovers}"
-    )
+    # output
+    print("\nSelected Flight:")
+    print(f"Flight Number: {selected_flight.flight_number}")
+    print(f"Distance: {selected_flight.distance} km")
+    print(f"Ticket Price: ${selected_flight.ticket_price}")
+    print(f"Duration: {selected_flight.duration} hours")
+    print(f"Layovers: {selected_flight.layovers}")
 
-    return sorted_data, selected_flight_info
+    # Visualize the dat
+    visualize_data(flights)
 
+if __name__ == "__main__":
+    main()
